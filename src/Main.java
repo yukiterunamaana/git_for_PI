5c2bcffbff57121bada32dee61ba1bebdbd424a3
Крамаренко Илья <kramarenko@sfedu.ru>
2023-11-16 16:54:43 +0300

    added bin to gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of Fibonacci numbers to generate: ");
+        int n = scanner.nextInt();
+
+        System.out.println("First " + n + " Fibonacci numbers:");
+
+        int[] fibonacciNumbers = new int[n];
+        fibonacciNumbers[0] = 0;
+        fibonacciNumbers[1] = 1;
+
+        // Generate Fibonacci numbers
+        for (int i = 2; i < n; i++) {
+            fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+        }
+
+        // Output Fibonacci numbers
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacciNumbers[i] + " ");
+        }
     }
 }
